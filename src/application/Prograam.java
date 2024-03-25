fa4c9b9c9dcdd0f1d8bbf20fdefd86748cdb9098
JoaoGabrielBonzanini <joaogabrielbonzanini@gmail.com>
2024-03-25 10:13:29 -0300

    First class: Position

--- src/application/Prograam.java
+++ src/application/Prograam.java
@@ -1,10 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Prograam {
 
 	public static void main(String[] args) {
 		
-
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 	}
 
 }
